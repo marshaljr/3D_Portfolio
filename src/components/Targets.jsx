8838a5f8bafab91225b05dca2192296fe7df6c75
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2025-12-02 21:14:26 +0545

    Comment out Targets component and imports
    Comment out the entire Targets component and related imports.

--- src/components/Targets.jsx
+++ src/components/Targets.jsx
@@ -1,42 +1,42 @@
-import { useGLTF } from "@react-three/drei";
-import React, { useRef, useEffect } from "react";
-import gsap from "gsap";
+// import { useGLTF } from "@react-three/drei";
+// import React, { useRef, useEffect } from "react";
+// import gsap from "gsap";
 
-useGLTF.preload(
-  "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf"
-);
+// useGLTF.preload(
+//   "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf"
+// );
 
-const Targets = (props) => {
-  const targetRef = useRef();
-  const { scene } = useGLTF(
-    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf"
-  );
+// const Targets = (props) => {
+//   const targetRef = useRef();
+//   const { scene } = useGLTF(
+//     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf"
+//   );
 
-  // useGSAP(() => {
-  //   gsap.to(targetRef.current.position, {
-  //     y: targetRef.current.position.y + 0.5,
-  //     duration: 1.5,
-  //     repeat: -1,
-  //     yoyo: true,
-  //   });
-  // });
-  useEffect(() => {
-    if (!targetRef.current) return;
+//   // useGSAP(() => {
+//   //   gsap.to(targetRef.current.position, {
+//   //     y: targetRef.current.position.y + 0.5,
+//   //     duration: 1.5,
+//   //     repeat: -1,
+//   //     yoyo: true,
+//   //   });
+//   // });
+//   useEffect(() => {
+//     if (!targetRef.current) return;
 
-    const tl = gsap.timeline({ repeat: -1, yoyo: true });
-    tl.to(targetRef.current.position, {
-      y: targetRef.current.position.y + 0.5,
-      duration: 1.5,
-    });
+//     const tl = gsap.timeline({ repeat: -1, yoyo: true });
+//     tl.to(targetRef.current.position, {
+//       y: targetRef.current.position.y + 0.5,
+//       duration: 1.5,
+//     });
 
-    return () => tl.kill();
-  }, []);
+//     return () => tl.kill();
+//   }, []);
 
-  return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-      <primitive object={scene} />
-    </mesh>
-  );
-};
+//   return (
+//     <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+//       <primitive object={scene} />
+//     </mesh>
+//   );
+// };
 
-export default Targets;
+// export default Targets;
